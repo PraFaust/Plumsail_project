58fe7bdf9316accc4c8f2edbcce1cd33159f2e20
PraFaust <maximnsuslov@gmail.com>
2020-11-28 17:32:25 +0300

    add navbar on sute

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,17 @@
 <template>
   <div id="app">
+    <b-navbar toggleable="lg" type="dark" variant="dark">
+      <b-navbar-brand href="#/" type="info">Home</b-navbar-brand>
+      <b-navbar-nav>
+        <b-nav-item href="#/contact" type="info">Contacts</b-nav-item>
+      </b-navbar-nav>
+
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item href="#/autorization" type="info">Login / Register</b-nav-item>
+      </b-navbar-nav>
+    </b-navbar>
+
     <router-view></router-view>
   </div>
 </template>
